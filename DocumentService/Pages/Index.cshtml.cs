39e04ec035184230bba27ee41616d2312bd34836
Boliang Chen <boliang.chen@sap.com>
2019-04-02 19:25:27 -0700

    add user

--- DocumentService/Pages/Index.cshtml.cs
+++ DocumentService/Pages/Index.cshtml.cs
@@ -13,5 +13,45 @@ namespace DocumentService.Pages
         {
 
         }
+
+        public IActionResult OnPostCrothall()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            Globals.CURRENT_USER = "Crothall";
+            return RedirectToPage("./Index");
+        }
+
+        public IActionResult OnPostHospitalA()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            Globals.CURRENT_USER = "Hospital A";
+            return RedirectToPage("./Index");
+        }
+
+        public IActionResult OnPostHospitalB()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            Globals.CURRENT_USER = "Hospital B";
+            return RedirectToPage("./Index");
+        }
+
+        public IActionResult OnPostHospitalC()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            Globals.CURRENT_USER = "Hospital C";
+            return RedirectToPage("./Index");
+        }
     }
 }
